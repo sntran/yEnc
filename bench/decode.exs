3a5f12e8a7f98507651d4eec537500fafc69705d
Son Tran-Nguyen <github@sntran.com>
2021-04-20 22:09:02 -0500

    docs(bench): add benchmarking
    Not as close to node-yenc, but there are improvements to be made.

---
+++ bench/decode.exs
@@ -0,0 +1,48 @@
+# Usage: mix run bench/decode.exs [optional switches]
+# Optional switches:
+#   -z, --size <bytes>        - Binary size in bytes to decode (default is 76800)
+#   -s, --sleep <seconds>     - warmup time before real benchmark, in seconds (default is 2)
+#   -j, --jobs <number>       - number of processes to be used for each benchmarking job (default is 1)
+#   -x, --extended_statistics - whether to show extended statistics
+#
+# Hit Ctrl+C twice to stop it.
+
+{options, _argv, _invalid} = OptionParser.parse(System.argv(),
+  aliases: [z: :size, s: :sleep, x: :extended_statistics],
+  strict: [size: :integer, sleep: :integer, extended_statistics: :boolean]
+)
+
+size = options[:size] || 768000
+
+jobs = %{
+  "yEnc" => &:yEnc.decode/1,
+  "YEnc" => &YEnc.decode/1,
+}
+
+{_microseconds, bufWorst} = :timer.tc(fn() ->
+  :yEnc.encode(:binary.copy(<<224>>, size))
+end)
+
+{_microseconds, bufBest} = :timer.tc(fn() ->
+  :yEnc.encode(:binary.copy(<<0>>, size))
+end)
+
+inputs = %{
+  "Worst (all escaping)" => bufWorst,
+  "Best (no escaping)" => bufBest,
+  "Random" => :yEnc.encode(Enum.at(StreamData.binary(length: size), 1))
+}
+
+Benchee.run(jobs,
+  parallel: options[:jobs] || 1,
+  warmup: options[:sleep] || 2,
+  time: 1,
+  # memory_time: 1,
+  inputs: inputs,
+  formatters: [
+    {
+      Benchee.Formatters.Console,
+      extended_statistics: options[:extended_statistics]
+    }
+  ]
+)
